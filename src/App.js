c7be402dcbd836d0ac1c1e0cfa7d1c6299c78f70
Fernando <valanife@gmail.com>
2021-10-30 15:07:00 -0300

    feat: page text create

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
 import "./assets/styles/App.css";
 import "./assets/styles/reset.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import Main from "./components/Main";
+import TextPage from "./components/TextPage";
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Main />
-      <Footer></Footer>
+      <BrowserRouter>
+        <Switch>
+          <Route path="/" exact>
+            <Header />
+            <Main />
+            <Footer />
+          </Route>
+          <Route path="/text" exact>
+            <Header />
+            <TextPage />
+            <Footer />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   );
 }
