622a1b068351bfd6c9b815b527c54ea1c111ecfd
Fernando <valanife@gmail.com>
2021-11-11 21:50:59 -0300

    feat: link update

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,25 @@
+import { useEffect, useState } from "react";
 import styled from "styled-components";
 import image from "../assets/images/background_header.png";
 
 export default function Header() {
+  const [scrollPosition, setScrollPosition] = useState(0);
+
+  const handleScroll = () => {
+    const position = window.pageYOffset;
+    setScrollPosition(position);
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
   return (
-    <Container>
+    <Container className="header" position={scrollPosition}>
       <div className="title">BCMT</div>
       <div className="sub-title">Bacharelado em Ciências Matemáticas e da Terra</div>
     </Container>
@@ -11,32 +27,71 @@ export default function Header() {
 }
 
 const Container = styled.div`
-  width: 100%;
-  height: 200px;
+  ${({ position }) => {
+    if (position >= 160) {
+      return `
+			background-image: url(${image});
+			background-position: center;
+			background-repeat: no-repeat;
+			background-size: cover;
+			background-attachment: fixed;
 
-  background-image: url(${image});
-  background-position: center;
-  background-size: cover;
+			max-width: 1210px;
+			width: 100%;
 
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  align-items: center;
+			position: fixed;
+			top: 0;
+			left: auto;
+			right: auto;
+			z-index: 1;
+			height: 50px;
 
-  box-shadow: 0px 2px 10px -2px #000;
+			display: flex;
+			align-items: center;
 
-  .title {
-    color: white;
-    font-size: 75px;
-    font-weight: bold;
-    line-height: 69px;
-  }
+			box-shadow: 0px 2px 10px -2px #000;
+			padding: 0 20px;
 
-  .sub-title {
-    width: 226px;
-    color: white;
+			.title {
+				color: #fff;
+				font-size: 35px;
+				font-weight: bold;
+			}
 
-    font-size: 17px;
-    text-align: center;
-  }
+			.sub-title {
+				display: none;
+			}
+		  `;
+    } else {
+      return `
+			width: 100%;
+			height: 200px;
+
+			background-image: url(${image});
+			background-position: center;
+			background-size: cover;
+
+			display: flex;
+			flex-direction: column;
+			justify-content: center;
+			align-items: center;
+
+			box-shadow: 0px 2px 10px -2px #000;
+
+			.title {
+				color: #fff;
+				font-size: 75px;
+				font-weight: bold;
+				line-height: 69px;
+			}
+
+			.sub-title {
+				width: 226px;
+				color: white;
+
+				font-size: 17px;
+				text-align: center;
+			}`;
+    }
+  }}
 `;
